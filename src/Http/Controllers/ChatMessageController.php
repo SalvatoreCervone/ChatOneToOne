ebbcc85250df982d6c97fe3a33ea0d66aaac9a8f
Salvatore <salvatore999@virgilio.it>
2024-08-07 12:54:53 +0200

    update component vue

---
+++ src/Http/Controllers/ChatMessageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+
+class ChatMessageController extends Controller
+{
+    function index(User $friend = null)
+    {
+        return  Inertia::render('Dashboard', [
+            'authuser' => auth()->user(),
+            // 'users' => User::whereNot('id', auth()->id())->get(),
+            // 'friend' => $friend
+
+        ]);
+    }
+}
---
+++ src/resources/js/Pages/Nuovomessaggio.vue
@@ -0,0 +1,9 @@
+<template>
+    <i v-if="newmessage" class="pi pi-comment"></i>
+</template>
+<script setup>
+import {  defineProps } from 'vue';
+const props = defineProps({
+    friend: null
+})
+</script>
